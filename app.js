e307395b2f20b4d4b859d279799f25461c1acbe1
Charles Riley <criles25@gmail.com>
2017-07-23 17:20:01 -0400

    Refresh token/signin endpoint working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var _ = require("lodash");
 var express = require("express");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
+var bearerToken = require("express-bearer-token");
 var HttpStatus = require("http-status-codes");
 
 var app = express();
@@ -9,8 +10,10 @@ var app = express();
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bearerToken());
 
 app.use(require("./routes/users"));
+app.use(require("./routes/token"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,19 @@
-var config = require("./../config/config");
-var bcrypt = require("bcrypt");
-var validate = require("express-validation");
-var Joi = require("joi");
-var jwt = require("jsonwebtoken");
-var knex = require("knex")({
+const config = require("./../config/config");
+const bcrypt = require("bcrypt");
+const validate = require("express-validation");
+const Joi = require("joi");
+const createToken = require("./../src/create-token");
+const knex = require("knex")({
   client: "postgresql",
   connection: config.connection_string || config.connection,
   pool: config.connection_pool
 });
+const express = require("express");
 
-var express = require("express");
-var router = express.Router();
+let router = express.Router();
 
-function createAccessToken(user) {
-  return jwt.sign(
-    {
-      iss: config.payload.issuer,
-      aud: config.payload.audience,
-      exp: Math.floor(Date.now() / 1000) + config.payload.expiration,
-      scope: config.payload.scope,
-      sub: config.payload.subject || user.username_lowercase,
-      jti: genJti(), // unique identifier for the token
-      alg: config.payload.algorithm,
-      role: config.payload.role
-    },
-    config.secret
-  );
-}
-
-// Generate Unique Identifier for the access token
-function genJti() {
-  let jti = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 16; i++) {
-    jti += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return jti;
-}
-
-/* Create account */
-var schema = {
+/* Create user */
+const schema = {
   body: {
     username: Joi.string().alphanum().min(3).max(20).required(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
@@ -49,49 +21,42 @@ var schema = {
   }
 };
 
-router.post("/users", validate(schema), function(req, res, next) {
-  // Check if user exists in db
-  return knex("api.users")
-    .where("username", req.body.username)
-    .orWhere("email", req.body.email)
-    .then(users => {
-      if (users.length > 0) {
-        var err = new Error(
-          "A user with that username or email already exists"
-        );
-        err.status = 400;
-        err.errors = [
-          { messages: ["A user with that username or email already exists"] }
-        ];
+router.post("/users", validate(schema), async function(req, res, next) {
+  let existingUsers = await knex("api.users")
+    .where("username_lowercase", req.body.username.toLowerCase())
+    .orWhere("email", req.body.email.toLowerCase());
 
-        return next(err);
-      }
+  if (existingUsers.length > 0) {
+    let err = new Error("A user with that username or email already exists");
+    err.status = 400;
+    err.errors = [
+      { messages: ["A user with that username or email already exists"] }
+    ];
 
-      // Insert into db
-      return bcrypt.hash(req.body.password, 10).then(function(hash) {
-        return knex("api.users")
-          .insert({
-            username: req.body.username,
-            username_lowercase: req.body.username.toLowerCase(),
-            email: req.body.email,
-            password: hash
-          })
-          .returning("*")
-          .then(users => {
-            return res.status(201).send({
-              access_token: createAccessToken(users[0])
-            });
-          });
-      });
-    })
-    .catch(err => {
-      err.status = 500;
-      err.errors = [
-        { messages: ["There was a problem creating your account"] }
-      ];
+    return next(err);
+  }
 
-      return next(err);
+  // Insert user into db
+  try {
+    let passDigest = await bcrypt.hash(req.body.password, 10);
+    let usersInserted = await knex("api.users")
+      .insert({
+        username: req.body.username,
+        username_lowercase: req.body.username.toLowerCase(),
+        email: req.body.email.toLowerCase(),
+        password: passDigest
+      })
+      .returning("*");
+
+    return res.status(201).send({
+      access_token: createToken(usersInserted[0])
     });
+  } catch (err) {
+    err.status = 500;
+    err.errors = [{ messages: ["There was a problem creating your account"] }];
+
+    return next(err);
+  }
 });
 
 module.exports = router;
