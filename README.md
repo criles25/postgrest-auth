c272953b9eed2a1e343bac6cc22305c42d08cd01
Charles Riley <criles25@gmail.com>
2017-08-03 22:44:24 -0400

    Added command line arguments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# postgrest-auth-server
+# postgrest-auth
+
+`postgrest-auth` is an authorization server for [PostgREST](https://github.com/begriffs/postgrest) written in [Node.js](https://github.com/nodejs/node).
+
+#### Usage
+```
+
+```
---
+++ config/config.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const argv = require("minimist")(process.argv.slice(2));
+
+let config = {
+  db: {
+    pool: {
+      min: 2,
+      max: 10
+    },
+    schema: "auth",
+    table: "users"
+  },
+  port: 3001,
+  roles: {
+    anonymous: "web_anon",
+    user: "reg_user"
+  }
+};
+
+// Merge config file
+if (argv.config || argv.c) {
+  _.merge(config, require(path.resolve(process.cwd(), argv.config || argv.c)));
+} else if (fs.existsSync(path.resolve(process.cwd(), "postgrest-auth.json"))) {
+  _.merge(config, require(path.resolve(process.cwd(), "postgrest-auth.json")));
+}
+
+if (argv.app || argv.A) {
+  config.app_name = argv.app || argv.a;
+}
+
+if (argv.connection || argv.C) {
+  config.db.connection_string = argv.connection || argv.C;
+}
+
+if (argv.schema || argv.s) {
+  config.db.schema = argv.schema || argv.s;
+}
+
+if (argv.table || argv.t) {
+  config.db.table = argv.db.table || argv.t;
+}
+
+if (argv.expire || argv.exp || argv.e) {
+  config.payload.exp = argv.expire || argv.exp || argv.e;
+}
+
+if (argv.issuer || argv.iss || argv.i) {
+  config.payload.iss = argv.issuer || argv.iss || argv.i;
+}
+
+if (argv.secret || argv.S) {
+  config.secret = argv.secret || argv.S;
+}
+
+if (argv.port || argv.p || process.env.PORT) {
+  config.port = argv.port || argv.p || process.env.PORT;
+}
+
+module.exports = config;
